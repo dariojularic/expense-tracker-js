564579bb806f7235d588a50a565893d1f65c2887
Dario Jularic <dariojularic0@gmail.com>
2024-05-30 21:38:12 +0200

    Add style

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // import './style.css'
+
+const totalBudget = document.querySelector(".total-budget");
+const totalExpenses = document.querySelector(".total-expenses");
+const totalBalance = document.querySelector(".total-balance");
+const inputBudget = document.querySelector(".input-budget");
+const inputTransactionPurpose = document.querySelector(".input-transaction-purpose");
+const inputTransactionCost = document.querySelector(".input-transaction-cost");
+const transactionList = document.querySelector(".transaction-list");
