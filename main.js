a656ffa1a8dacf06084b7359faf7f02af8928fcf
Dario Jularic <dariojularic0@gmail.com>
2024-06-03 18:14:27 +0200

    Create classes

--- main.js
+++ main.js
@@ -8,3 +8,62 @@ const inputTransactionPurpose = document.querySelector(".input-transaction-purpo
 const inputTransactionCost = document.querySelector(".input-transaction-cost");
 const transactionList = document.querySelector(".transaction-list");
 
+class Transaction{
+  constructor(cost, purpose) {
+    this.cost = cost
+    this.purpose = purpose
+    this.id = self.crypto.randomUUID()
+    // this.day = 
+    // this.time = 
+    // kako pisat id?
+  }
+}
+
+class TransactionManager{
+  constructor() {
+    this.transactionArray = []
+  }
+
+  addTransaction(transaction) {
+    this.transactionArray.push(transaction)
+  }
+
+  removeTransaction(transactionId) {
+    this.transactionArray.filter(transaction => transaction.id !== transactionId)
+  }
+
+  renderTransactions() {
+    // jos doradit ovu funkciju
+    transactionList.innerHTML = ""
+    this.transactionArray.forEach(transaction => {
+      const html = `<li>${transaction.purpose} ${transaction.cost} <button>Remove</button></li>`;
+      transactionList.insertAdjacentElement("afterbegin", html)
+    })
+
+  }
+}
+
+// budget state, balance, expenses array
+// expenses novi class  expenses manager
+// budget manager total budget, total expenses, total balance
+
+class BudgetManager{
+  constructor(totalBudget) {
+    this.totalBudget = totalBudget
+    this.totalExpenses = 0
+    // sta dodijelit na totalBalance
+    this.totalBalance = totalBudget
+  }
+
+  // pregledat ovu funkciju
+  calculateExpenses(transactionsArray) {
+    this.totalExpenses = 0
+    transactionsArray.forEach(transaction => {
+      this.totalExpenses += transaction.cost
+    })
+  }
+
+  setBalance() {
+    this.totalBalance = this.totalBudget - this.totalExpenses
+  }
+}
